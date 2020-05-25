5598accfacb4a65d875c3e12f9d203615d7857f3
taizo-pro <t2s.single@gmail.com>
2020-05-25 22:35:42 +0900

    Initial Commit

---
+++ testAdRemove/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testAdRemove
+//
+//  Created by Kazuki Harada on 2020/05/25.
+//  Copyright © 2020 Harada Kazuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
